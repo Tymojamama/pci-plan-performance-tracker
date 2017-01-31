59c9d6a55ceb6da84285f019bedb808fd4a67d74
Zach417 <zallen@pension-consultants.com>
2017-01-31 14:39:40 -0600

    added plan object to database

---
+++ plan-performance-tracker.Business/Entities/Plan.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using PlanPerformance.Business.Components;
+using PensionConsultants.Data.Utilities;
+
+namespace PlanPerformance.Business.Entities
+{
+    public class Plan : DatabaseEntity
+    {
+        public string ActionPlan;
+
+        private static string _tableName = "PlanMetric";
+
+        public Plan()
+            : base(_tableName)
+        {
+
+        }
+
+        public Plan(Guid primaryKey)
+            : base(_tableName, primaryKey)
+        {
+            RefreshMembers();
+        }
+
+        /// <summary>
+        /// Registers the instance's members with the abstract class in order to perform database operations. Do not register members
+        /// that exist within the abstract class (e.g. CreatedOn).
+        /// </summary>
+        protected override void RegisterMembers()
+        {
+            base.AddColumn("ActionPlan", this.ActionPlan);
+        }
+
+        /// <summary>
+        /// Resets the values of all public members to their values in the database.
+        /// </summary>
+        protected override void SetRegisteredMembers()
+        {
+            this.ActionPlan = (String)base.GetColumn("ActionPlan");
+        }
+    }
+}
