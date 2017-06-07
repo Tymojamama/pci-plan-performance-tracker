3a098636c2ff613b2979179f507e850194b59413
Zach Allen <zallen@pension-consultants.com>
2017-06-07 10:32:14 -0500

    fixed plan metrics view bug

--- plan-performance-tracker.Web/Controllers/AccountController.cs
+++ plan-performance-tracker.Web/Controllers/AccountController.cs
@@ -240,7 +240,7 @@ namespace PlanPerformance.Web.Controllers
                 string code = await UserManager.GeneratePasswordResetTokenAsync(user.Id);
                 var callbackUrl = Url.Action("ResetPassword", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
                 //await UserManager.SendEmail(user.Id, "Reset Password", "Please reset your password by clicking <a href=\"" + callbackUrl + "\">here</a>");
-                UserManager.SendEmail(user.Id, "Reset Password", "Please reset your password by clicking <a href=\"" + callbackUrl + "\">here</a>");
+                UserManager.SendEmail(user.Id, "Reset Password", "Please reset your password by clicking here: " + callbackUrl);
                 return RedirectToAction("ForgotPasswordConfirmation", "Account");
             }
 
